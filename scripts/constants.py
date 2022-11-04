8040d0c1e6a52ebad7b28c43e2bdabef092ced6b
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-03 20:23:00 -0400

    Add more constants

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,9 +1,12 @@
 def get_constants():
     return {
         'len_seed': 100,
-        'mnist_size': 28,
+        'mnist_width': 28,
+        'mnist_height': 28,
         'mnist_channels': 1,
+        'mnist_batch_size': 100,
         'scapes_width': 1280,
         'scapes_height': 720,
         'scapes_channels': 3,
+        'scapes_batch_size': 100
     }
