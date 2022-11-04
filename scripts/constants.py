d28148f4fbe4395821b3b60d3b228df94a17b035
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-03 20:20:18 -0400

    Add fill with constants

---
+++ scripts/constants.py
@@ -0,0 +1,9 @@
+def get_constants():
+    return {
+        'len_seed': 100,
+        'mnist_size': 28,
+        'mnist_channels': 1,
+        'scapes_width': 1280,
+        'scapes_height': 720,
+        'scapes_channels': 3,
+    }
