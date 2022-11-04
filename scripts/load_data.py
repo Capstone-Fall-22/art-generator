44a2a6d501fefeff432ac0b27ca63f9fed8bb6d0
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-03 20:03:17 -0400

    Create function to load dataset

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,9 +1,16 @@
-import tensorflow_datasets as tfds
+import tensorflow as tf
 
-def load_dataset(name_or_path, tfds_dataset=False):
-    # If tfds_dataset is True, then name_or_path is the name of a dataset in the TensorFlow Datasets library
-    # Otherwise, name_or_path is the path to a directory containing a dataset
-    if tfds_dataset:
-        return tfds.load(name_or_path, shuffle_files=True)
-
-print(load_dataset('mnist', tfds_dataset=True))
+def load_dataset(name, batch_size, image_dimensions):
+    return tf.keras.utils.image_dataset_from_directory(
+        directory=f'/content/{name}', 
+        labels=None, 
+        color_mode='rgb', 
+        batch_size=batch_size, 
+        image_size=(image_dimensions[1], image_dimensions[0]), 
+        shuffle=True, 
+        seed=42, 
+        validation_split=None, 
+        subset=None, 
+        interpolation='bilinear', 
+        follow_links=False
+    )
