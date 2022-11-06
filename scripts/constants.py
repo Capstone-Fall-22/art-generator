1ffbe24d64d086903bb966de2d05e7fa14658e9c
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-06 13:01:51 -0500

    Add new constants dict structure

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,12 +1,21 @@
 def get_constants():
     return {
         'len_seed': 100,
-        'mnist_width': 28,
-        'mnist_height': 28,
-        'mnist_channels': 1,
-        'mnist_batch_size': 100,
-        'scapes_width': 1280,
-        'scapes_height': 720,
-        'scapes_channels': 3,
-        'scapes_batch_size': 100
+        'random_seed': 42,
+        'mnist': {
+            'width': 28,
+            'height': 28,
+            'color_mode': 'grayscale',
+            'batch_size': 100,
+            'local': '../data/mnist',
+            'colab': '/content/mnist'
+        },
+        'scapes': {
+            'width': 1280,
+            'height': 720,
+            'channels': 'rgb',
+            'batch_size': 100,
+            'local': '../data/scapes',
+            'colab': '/content/scapes'
+        }
     }
