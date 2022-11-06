b1cb0b43adfa0afe61a5647360c1a538c868e794
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-06 12:58:01 -0500

    Changed code for loading dataset to be easier to use in an experimentation notebook

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
+from constants import get_constants
 
-def load_dataset(name, batch_size, image_dimensions):
+def load_dataset(name, colab=False):
+    constants = get_constants()
+    seed = constants['random_seed']
+    constants = constants[name]
+
     return tf.keras.utils.image_dataset_from_directory(
-        directory=f'/content/{name}', 
+        directory=constants['colab'] if colab else constants['local'], 
         labels=None, 
-        color_mode='rgb', 
-        batch_size=batch_size, 
-        image_size=(image_dimensions[1], image_dimensions[0]), 
+        color_mode=constants[name]['color_mode'], 
+        batch_size=constants['batch_size'], 
+        image_size=(constants['width'], constants['height']), 
         shuffle=True, 
-        seed=42, 
+        seed=seed, 
         validation_split=None, 
         subset=None, 
         interpolation='bilinear', 
         follow_links=False
     )
+
+print(load_dataset('mnist'))
