278fcec1ea21078ff64a325618effa9814f630c3
Chen Stanilovsky <chen.stanilovsky@gmail.com>
2022-11-13 17:02:17 -0500

    Add new dataset configs

--- scripts/resize.py
+++ scripts/resize.py
@@ -4,16 +4,8 @@ from concurrent.futures import ThreadPoolExecutor
 from resize_config import get_resize_config
 
 
-def imageFitsInDesiredSize(image, width, height):
-    return image.shape[0] <= height and image.shape[1] <= width
-
-
-def resize_image(
-    image_path, output_path, width, height, rescale_factor=0.1, zero_pad_limit=0.5
-):
-    # print('Resizing image: {}'.format(image_path))
-    image = cv2.imread(image_path)
-    if imageFitsInDesiredSize(image, width, height):
+def scale_image(image, width, height, rescale_factor):
+    if image_fits_in_desired_size(image, width, height):
         # Grow image
         if image.shape[0] > image.shape[1]:
             while image.shape[0] < height:
@@ -42,17 +34,40 @@ def resize_image(
         # Reverse the last resize
         image = cv2.resize(image, (0, 0), fx=1 + rescale_factor, fy=1 + rescale_factor)
 
-    # Crop image
-    image = image[0:height, 0:width]
+    return image
+
+def image_fits_in_desired_size(image, width, height):
+    return image.shape[0] <= height and image.shape[1] <= width
+
+
+def resize_image(
+    image_path, output_path, width, height, rescale_factor=0.1, zero_pad_limit=0.5
+):
+    image = cv2.imread(image_path)
+
+    if not config["crop"]:
+        image = scale_image(image, width, height, rescale_factor)
+
+
+    # Crop image from center
+    if config["crop"] and image.shape[0] > height:
+        start_height = int((image.shape[0] - height) / 2)
+        end_height = start_height + height
+        image = image[start_height:end_height, :]
+
+    if config["crop"] and image.shape[1] > width:
+        start_width = int((image.shape[1] - width) / 2)
+        end_width = start_width + width
+        image = image[:, start_width:end_width]
+
 
     # Zero pad image
     if image.shape[0] < height or image.shape[1] < width:
-        # Add 0 from top, desirsed_height - actual_height for bottom, 0 for left, desired_width - actual_width for right
         if (
-            height / image.shape[0] > zero_pad_limit
-            or width / image.shape[1] > zero_pad_limit
+            1 - (image.shape[0] / height) > zero_pad_limit
+            or 1 - (image.shape[1] / width) > zero_pad_limit
         ):
-            # Delete image which is going to be padded more than the limit
+            # Skip image which is going to be padded more than the limit
             return
 
         image = cv2.copyMakeBorder(
@@ -89,6 +104,8 @@ def get_resize_image_args(config):
             if image == "attributions.json":
                 continue
             image_path = os.path.join(category_dir, image)
+            # Save images as PNG for lossless compression
+            image = ".".join(image.split(".")[:-1]) + ".png"
             output_image_path = os.path.join(config["output_path"], category, image)
             args.append(
                 (
@@ -118,13 +135,13 @@ def check_image_size(image_path, width, height):
 
 if __name__ == "__main__":
     config = get_resize_config()
-    prepare_output_dirs(config)
-    resize_image_args = get_resize_image_args(config)
-    with ThreadPoolExecutor() as executor:
-        # We unpack the arguments here because the zip function expects a comma separated list of arguments
-        # We unpack the result of the zip function because the map function expects a comma separated list of arguments
-        # e.g. map(func, (arg1, arg2), (arg1, arg2)) and zip((arg1, arg1), (arg2, arg2))
-        _ = executor.map(resize_image, *zip(*resize_image_args))
+    # prepare_output_dirs(config)
+    # resize_image_args = get_resize_image_args(config)
+    # with ThreadPoolExecutor() as executor:
+    #     # We unpack the arguments here because the zip function expects a comma separated list of arguments
+    #     # We unpack the result of the zip function because the map function expects a comma separated list of arguments
+    #     # e.g. map(func, (arg1, arg2), (arg1, arg2)) and zip((arg1, arg1), (arg2, arg2))
+    #     _ = executor.map(resize_image, *zip(*resize_image_args))
 
     check_image_size_args = []
     for category in os.listdir(config["output_path"]):
--- scripts/resize_config.py
+++ scripts/resize_config.py
@@ -1,9 +1,10 @@
 def get_resize_config():
     return {
-        "input_path": "./experiments/data/full_sized_dataset",
-        "output_path": "./experiments/data/scapes_resized_cleaned",
-        "width": 1280,
-        "height": 720,
+        "input_path": "/home/chen/big_bertha/coding/capstone/art-generator/experiments/data/original_full_sized_scapes",
+        "output_path": "/home/chen/big_bertha/coding/capstone/art-generator/experiments/data/scapes_640_320_cropped_zero_pad_limit_0_5",
+        "width": 640,
+        "height": 360,
         "resize_factor": 0.01,
-        "zero_pad_limit": 0.1,
+        "zero_pad_limit": 0.5,
+        "crop": True
     }
